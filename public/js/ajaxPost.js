ef9e7e852c72edb24bb5b52fdfb0993ec5ee6b1d
hougaoping_thinkpc <hougaoping@163.com>
2018-07-25 00:34:10 +0800

    优化Ajaxpost，使用匿名回调函数

--- public/js/ajaxPost.js
+++ public/js/ajaxPost.js
@@ -1,39 +1,3 @@
-// 默认的回调函数，用于关闭弹出窗口
-var _defaultAjaxCallbackSuccess = function(return_data){
-    if (return_data.data.direct == false || return_data.url == '') {
-        return_data.url = undefined;
-    }
-    
-    $.message({
-        type: 'success',
-        html: return_data.message,
-        showHtml: function(el, time) {
-                        // el.html(return_data.message + ' ' + time);
-                        el.html(return_data.message);
-                    },
-        url: return_data.url,
-        timeout: return_data.wait * 1000
-    });
-};
-
-var _defaultAjaxCallbackError = function(return_data) {
-    // 显示laravel返回的错误列表中的第一条
-    if (return_data.errors) {
-        $.each(return_data.errors, function(idx, obj) {
-            $.message({
-                type: 'error',
-                html: obj[0],
-            });
-            return false;
-        });
-    }else {
-        $.message({
-            type: 'error',
-            html: return_data.message
-        });  
-    }
-}
-
 function ajaxPost(form, callbackSuccess, callbackError) {
     return $.ajax({
         url: form.attr('action'),
@@ -42,11 +6,42 @@ function ajaxPost(form, callbackSuccess, callbackError) {
         cache: false,
         data: form.serialize(),
         success: function(return_data) {
-            callbackSuccess ? callbackSuccess(return_data) : _defaultAjaxCallbackSuccess(return_data);
+            callbackSuccess ? callbackSuccess(return_data) : (function(return_data) {
+                if (return_data.data.direct == false || return_data.url == '') {
+                    return_data.url = undefined;
+                }
+                
+                $.message({
+                    type: 'success',
+                    html: return_data.message,
+                    showHtml: function(el, time) {
+                                    // el.html(return_data.message + ' ' + time);
+                                    el.html(return_data.message);
+                                },
+                    url: return_data.url,
+                    timeout: return_data.wait * 1000
+                });
+            })(return_data);
         },
         error: function(return_data) {
             var json = JSON.parse(return_data.responseText);
-            callbackError ? callbackError(json) : _defaultAjaxCallbackError(json);
+            callbackError ? callbackError(json) : (function(return_data) {
+                // 显示laravel返回的错误列表中的第一条
+                if (return_data.errors) {
+                    $.each(return_data.errors, function(idx, obj) {
+                        $.message({
+                            type: 'error',
+                            html: obj[0],
+                        });
+                        return false;
+                    });
+                }else {
+                    $.message({
+                        type: 'error',
+                        html: return_data.message
+                    });  
+                }
+            })(json);
         }
     });
 }
--- resources/views/admin/layouts/_header.blade.php
+++ resources/views/admin/layouts/_header.blade.php
@@ -1,13 +1,13 @@
 <div class="header d-flex justify-content-between align-items-center"">
     <span id="expand-sidebar" title="折叠菜单"></span>
-    <div class="header-nav dropdown">
+    <div class="header-nav dropdown mr-4">
         <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
           {{ Auth::user()->getUsername(false)}}
         </button>
         <div class="dropdown-menu">
           <?php
             $roles = Auth::user()->getAdminRolesName();
-            isset($roles[0]) ? '<a class="dropdown-item disabled" href="">' . $roles[0] . '</a>' : '';
+            echo isset($roles[0]) ? '<a class="dropdown-item disabled" href="">' . $roles[0] . '</a>' : '';
           ?>
           <a class="dropdown-item" href="{{ route('center.password') }}" target="_blank">修改我的密码</a>
           <a class="dropdown-item" href="{{ route('logout') }}">退出</a>
