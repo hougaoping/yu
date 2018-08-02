14ef314d0b9c08041763373d201f24b5ad14d764
hougaoping_thinkpc <hougaoping@163.com>
2018-08-02 23:41:26 +0800

    细节修改

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,44 +1,44 @@
 @extends('admin.layouts.admin')
 @section('content')
-<div class="card">
-	<div class="card-header">系统信息</div>
-	<div class="table-responsive">
-		<table class="table table-hover">
-			<tbody>
-			<tr>
-			  <td class="text-right text-muted">上传文件大小限制：</td>
-			  <td>{{ $sys_info['upload_max_filesize'] }}</td>
-			  <td class="text-right text-muted">服务器系统：</td>
-			  <td>{{ $sys_info['os'] }}</td>
-			  <td class="text-right text-muted">数据库版本：</td>
-			  <td>{{ $sys_info['mysql_version'] }}</td>
-			</tr>
-			<tr>
-			  <td class="text-right text-muted">是否支持REWRITE重写：</td>
-			  <td>{{ $sys_info['rewrite_module'] }}</td>
-			  <td class="text-right text-muted">SOCKET支持：</td>
-			  <td>{{ $sys_info['socket'] }}</td>
-			  <td class="text-right text-muted">服务器软件：</td>
-			  <td>{{ $sys_info['webserver'] }} </td>
-			</tr>
-			<tr>
-			  <td class="text-right text-muted">GD图形库支持：</td>
-			  <td>{{ $sys_info['gd'] }}</td>
-			  <td class="text-right text-muted">服务器所在时区：</td>
-			  <td>{{ $sys_info['timezone'] }}</td>
-			  <td class="text-right text-muted">服务器时间：</td>
-			  <td>{{ $sys_info['date'] }}</td>
-			</tr>
-			<tr>
-			  <td class="text-right text-muted">PHP版本信息：</td>
-			  <td><a href="{{ action('Admin\DashboardController@index', ['phpinfo'=>'yes']) }}" target="_blank">查看</a></td>
-			  <td class="text-right text-muted">占用内存：</td>
-			  <td>{{ $sys_info['memory_info'] }}</td>
-			  <td class="text-right text-muted">服务器空间占用：</td>
-			  <td>{!! $sys_info['dirsize'] !!}</td>
-			</tr>
-			</tbody>
-		</table>
+	<div class="card">
+		<div class="card-header">系统信息</div>
+		<div class="table-responsive">
+			<table class="table table-hover" style="min-width:900px">
+				<tbody>
+				<tr>
+				  <td class="text-right text-muted">上传文件大小限制：</td>
+				  <td>{{ $sys_info['upload_max_filesize'] }}</td>
+				  <td class="text-right text-muted">服务器系统：</td>
+				  <td>{{ $sys_info['os'] }}</td>
+				  <td class="text-right text-muted">数据库版本：</td>
+				  <td>{{ $sys_info['mysql_version'] }}</td>
+				</tr>
+				<tr>
+				  <td class="text-right text-muted">是否支持REWRITE重写：</td>
+				  <td>{{ $sys_info['rewrite_module'] }}</td>
+				  <td class="text-right text-muted">SOCKET支持：</td>
+				  <td>{{ $sys_info['socket'] }}</td>
+				  <td class="text-right text-muted">服务器软件：</td>
+				  <td>{{ $sys_info['webserver'] }} </td>
+				</tr>
+				<tr>
+				  <td class="text-right text-muted">GD图形库支持：</td>
+				  <td>{{ $sys_info['gd'] }}</td>
+				  <td class="text-right text-muted">服务器所在时区：</td>
+				  <td>{{ $sys_info['timezone'] }}</td>
+				  <td class="text-right text-muted">服务器时间：</td>
+				  <td>{{ $sys_info['date'] }}</td>
+				</tr>
+				<tr>
+				  <td class="text-right text-muted">PHP版本信息：</td>
+				  <td><a href="{{ action('Admin\DashboardController@index', ['phpinfo'=>'yes']) }}" target="_blank">查看</a></td>
+				  <td class="text-right text-muted">占用内存：</td>
+				  <td>{{ $sys_info['memory_info'] }}</td>
+				  <td class="text-right text-muted">服务器空间占用：</td>
+				  <td>{!! $sys_info['dirsize'] !!}</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
 @stop
--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,133 +1,133 @@
 @extends('admin.layouts.admin')
 
 @section('content')
-<div class="row">
-	<div class="col-md-6 col-12">
-	<div class="card mb-4">
-		<div class="card-header">注册信息</div>
-		<div class="show">
-			<table class="table table-hover">
-				<colgroup>
-					<col style="width:30%">
-					<col style="width:auto">
-				</colgroup>
-				<tbody>
-				<tr class="">
-					<td class="text-right text-muted">电子邮箱：</td>
-					<td class="text-md-left">{{ $user['email'] }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">手机号码：</td>
-					<td class="text-md-left">{{ $user['mobile'] }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">注册时间：</td>
-					<td class="text-md-left">{{ date('Y-m-d H:i:s',$user['register_time']) }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">最后一次登录时间：</td>
-					<td class="text-md-left">{{ date('Y-m-d H:i:s',$user['last_login_time']) }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">注册IP地址：</td>
-					<td class="text-md-left">{{ $user['register_ip'] }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">最后一次登录IP地址：</td>
-					<td class="text-md-left">{{ $user['last_login_ip'] }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">会员类型：</td>
-					<td class="text-md-left">{{ $user['type'] == 'seller' ? '商家会员' : '买家会员' }}</td>
-				</tr>
-				<tr class="">
-					<td class="text-right text-muted">登录次数：</td>
-					<td class="text-md-left">{{ $user['login_count'] }}</td>
-				</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
-	</div>
-	<div class="col-md-6 col-12">
-		<div class="card">
-			<div class="card-header">个人信息</div>
-			<div class="show">
-				<table class="table table-hover">
-					<colgroup>
-						<col style="width:30%">
-						<col style="width:auto">
-					</colgroup>
-					<tbody>
-					<tr class="">
-						<td class="text-right text-muted">姓名：</td>
-						<td class="text-md-left">{{ $profile['name'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">性别：</td>
-						<td class="text-md-left">
-							<?php
-								switch($profile['gender']) {
-									case 'male':
-										echo '男';
-										break;
-									case 'female':
-										echo '女';
-										break;
-									default:
-										echo '';
-										break;
-								}
-							?>
-						</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">手机号码：</td>
-						<td class="text-md-left">{{ $profile['mobile'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">电子邮箱：</td>
-						<td class="text-md-left">{{ $profile['email'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">QQ号码：</td>
-						<td class="text-md-left">{{ $profile['qq'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">微信号码：</td>
-						<td class="text-md-left">{{ $profile['wx'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">地址：</td>
-						<td class="text-md-left">
-							<div class="row">
-								<div class="col-4">
-									<select class="form-control address disable" id="address" disabled><option>请选择</option></select>
-								</div>
-								<div class="col-4">
-									<select class="form-control address" id="city" disabled><option>请选择</option></select>
-								</div>
-								<div class="col-4">
-									<select class="form-control address" id="area" disabled><option>请选择</option></select>
-								</div>
-							</div>
-							<input type="hidden" name="address" value="@isset($profile['address']){{ $profile['address'] }}@endisset" id="_address">
-						</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">详细地址：</td>
-						<td class="text-md-left">{{ $profile['complete_address'] }}</td>
-					</tr>
-					<tr class="">
-						<td class="text-right text-muted">个人简介：</td>
-						<td class="text-md-left">{{ $profile['intro'] }}</td>
-					</tr>
-					</tbody>
-				</table>
+	<div class="row">
+		<div class="col-md-6 col-12">
+			<div class="card mb-4">
+				<div class="card-header">注册信息</div>
+				<div class="show">
+					<table class="table table-hover">
+						<colgroup>
+							<col style="width:30%">
+							<col style="width:auto">
+						</colgroup>
+						<tbody>
+						<tr class="">
+							<td class="text-right text-muted">电子邮箱：</td>
+							<td class="text-md-left">{{ $user['email'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">手机号码：</td>
+							<td class="text-md-left">{{ $user['mobile'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">注册时间：</td>
+							<td class="text-md-left">{{ date('Y-m-d H:i:s',$user['register_time']) }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">最后一次登录时间：</td>
+							<td class="text-md-left">{{ date('Y-m-d H:i:s',$user['last_login_time']) }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">注册IP地址：</td>
+							<td class="text-md-left">{{ $user['register_ip'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">最后一次登录IP地址：</td>
+							<td class="text-md-left">{{ $user['last_login_ip'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">会员类型：</td>
+							<td class="text-md-left">{{ $user['type'] == 'seller' ? '商家会员' : '买家会员' }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">登录次数：</td>
+							<td class="text-md-left">{{ $user['login_count'] }}</td>
+						</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
-</div>
+		<div class="col-md-6 col-12">
+			<div class="card">
+				<div class="card-header">个人信息</div>
+				<div class="show">
+					<table class="table table-hover">
+						<colgroup>
+							<col style="width:30%">
+							<col style="width:auto">
+						</colgroup>
+						<tbody>
+						<tr class="">
+							<td class="text-right text-muted">姓名：</td>
+							<td class="text-md-left">{{ $profile['name'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">性别：</td>
+							<td class="text-md-left">
+								<?php
+									switch($profile['gender']) {
+										case 'male':
+											echo '男';
+											break;
+										case 'female':
+											echo '女';
+											break;
+										default:
+											echo '';
+											break;
+									}
+								?>
+							</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">手机号码：</td>
+							<td class="text-md-left">{{ $profile['mobile'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">电子邮箱：</td>
+							<td class="text-md-left">{{ $profile['email'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">QQ号码：</td>
+							<td class="text-md-left">{{ $profile['qq'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">微信号码：</td>
+							<td class="text-md-left">{{ $profile['wx'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">地址：</td>
+							<td class="text-md-left">
+								<div class="row">
+									<div class="col-4">
+										<select class="form-control address disable" id="address" disabled><option>请选择</option></select>
+									</div>
+									<div class="col-4">
+										<select class="form-control address" id="city" disabled><option>请选择</option></select>
+									</div>
+									<div class="col-4">
+										<select class="form-control address" id="area" disabled><option>请选择</option></select>
+									</div>
+								</div>
+								<input type="hidden" name="address" value="@isset($profile['address']){{ $profile['address'] }}@endisset" id="_address">
+							</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">详细地址：</td>
+							<td class="text-md-left">{{ $profile['complete_address'] }}</td>
+						</tr>
+						<tr class="">
+							<td class="text-right text-muted">个人简介：</td>
+							<td class="text-md-left">{{ $profile['intro'] }}</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
 
 @push('links')
